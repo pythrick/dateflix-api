0f7334c810e7ac75899026165da411e32cb6b1f4
Pythrick <patrhick@gmail.com>
2020-07-31 10:42:08 -0300

    Consume Instagram Pictures and save in database

--- dateflix_api/app/models/picture.py
+++ dateflix_api/app/models/picture.py
@@ -4,5 +4,5 @@ from .mixin import ModelMixin
 
 
 class Picture(ModelMixin):
-    url = models.URLField(blank=False, null=False)
+    url = models.URLField(blank=False, null=False, max_length=300)
     user = models.ForeignKey("User", models.CASCADE, related_name="pictures")
--- dateflix_api/app/views/user.py
+++ dateflix_api/app/views/user.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import get_user_model
+from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import mixins, status, viewsets
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from dateflix_api.app.models import User
+from dateflix_api.app.models import Picture, User
 from dateflix_api.app.serializers import TokenObtainPairSerializer, UserSerializer
 from dateflix_api.app.services import instagram
 
@@ -19,19 +20,30 @@ class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         response = instagram.authenticate(serializer.validated_data.pop("code"))
-        serializer.validated_data.update({"instagram_user_id": response.get("user_id")})
+        user_id = response.get("user_id")
+        access_token = response.get("access_token")
+
+        serializer.validated_data.update({"instagram_user_id": user_id})
 
         User = get_user_model()
-
         if User.objects.filter(instagram_user_id=response.get("user_id")).exists():
             raise PermissionDenied(detail=_("User already registered."))
 
         user = User.objects.create(**serializer.validated_data)
+
+        # Get pictures URLs and store them in the Database
+        profile = instagram.get_profile(access_token)
+        images_ids = [d["id"] for d in profile["media"]["data"]]
+        pictures = instagram.get_pictures(access_token, images_ids)
+        for picture in pictures:
+            Picture.objects.create(url=picture, user=user)
+
         refresh = TokenObtainPairSerializer.get_token(user)
         data = {"refresh": str(refresh), "access": str(refresh.access_token)}
         return Response(data, status=status.HTTP_201_CREATED)
